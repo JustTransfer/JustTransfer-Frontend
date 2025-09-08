a8918b0d05a282f3cf2353ade238608a65406524
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2025-09-08 22:50:17 +0200

    feat: add libsoduim

--- app/handlers/api.tsx
+++ app/handlers/api.tsx
@@ -5,25 +5,47 @@ const apiUrl = "http://localhost:3000";
 async function registerStartAPI(username: string, client_registration_start: string): Promise<Response> {
 
     const response = await fetch(`${apiUrl}/register/start`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      client_registration_start,
-    }),
-  });
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            client_registration_start,
+        }),
+    });
 
-  if (!response.ok) {
-    throw new Error(`Error: ${response.status} ${response.statusText}`);
-  }
+    if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
 
-  return (await response.json());
+    return (await response.json());
 }
 
-function registerEndAPI() {
+async function registerEndAPI(username: string, client_registration_finish: string, cpriv_enc: string, nonce_priv_enc: string, pub_enc: string, cpriv_sign: string, nonce_priv_sign: string, pub_sign: string) {
 
+    const response = await fetch(`${apiUrl}/register/end`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            client_registration_finish,
+            cpriv_enc,
+            nonce_priv_enc,
+            pub_enc,
+            cpriv_sign,
+            nonce_priv_sign,
+            pub_sign,
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    return (await response.json());
 }
 
 function registerUpdateAPI() {
--- app/handlers/crypto.tsx
+++ app/handlers/crypto.tsx
@@ -1,4 +1,7 @@
 import * as opaque from "@serenity-kit/opaque";
+import sodium from "libsodium-wrappers-sumo";
+import { Base64 } from 'js-base64';
+
 import { registerStartAPI, registerEndAPI, registerUpdateAPI, loginStartAPI, loginEndAPI, logoutAPI, getPublicKeyEncAPI, getPublicKeySignAPI, getMessagesAPI, sendMessageAPI } from "./api";
 
 async function register(username: string, email: string, password: string) {
@@ -6,10 +9,47 @@ async function register(username: string, email: string, password: string) {
 
     const { clientRegistrationState, registrationRequest } = opaque.client.startRegistration({ password });
 
-    const result = await registerStartAPI(username, registrationRequest);
+    const response = await registerStartAPI(username, registrationRequest);
 
-    console.log("Return code:", result.status);
-    console.log("Response from /register/start:", result);
+    console.log("Return code:", response.status);
+    console.log("Response from /register/start:", response);
+
+    const registrationResponse = response.result;
+
+    const { exportKey, registrationRecord } = opaque.client.finishRegistration({
+        clientRegistrationState,
+        registrationResponse,
+        password,
+    });
+
+    // Decode it from base64Url
+    const exportKeyDecoded = Base64.toUint8Array(exportKey).slice(0, 32); // Take only first 32 bytes
+    console.log("Export key decoded:", exportKeyDecoded);
+
+    // Init libsodium TODO change place
+    await sodium.ready;
+
+    // Generate encryption key pair
+    const KeyPairEnc = sodium.crypto_box_keypair();
+    const PublicKeyEnc = KeyPairEnc.publicKey;
+    const PrivateKeyEnc = KeyPairEnc.privateKey;
+
+    // Encrypt private key
+    const nonce_enc = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
+    const cpriv_enc = sodium.crypto_secretbox_easy(PrivateKeyEnc, nonce_enc, exportKeyDecoded);
+
+
+    // Generate signing key pair
+    const KeyPairSign = sodium.crypto_box_keypair();
+    const PublicKeySign = KeyPairSign.publicKey;
+    const PrivateKeySign = KeyPairSign.privateKey;
+
+    // Encrypt private key
+    const nonce_sign = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
+    const cpriv_sign = sodium.crypto_secretbox_easy(PrivateKeySign, nonce_sign, exportKeyDecoded);
+
+    const result2 = await registerEndAPI(username, registrationRecord, cpriv_enc, nonce_enc, PublicKeyEnc, cpriv_sign, nonce_sign, PublicKeySign);
+
 }
 
 async function login(email: string, password: string) {
