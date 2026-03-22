fbc31ecfbc8e7dc958be937cc40575ac232cfcb5
Justin Ferrara <justin.ferrara@heig-vd.ch>
2026-03-22 20:18:49 +0100

    feat: add auto scroll top

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
+import ScrollToTop from './components/scrollToTop';
 import { NotificationProvider } from './hooks/useNotificationContext';
 import { ServerConfigProvider } from './hooks/useServerConfig';
 import { AuthProvider } from "./hooks/useAuth";
@@ -27,6 +28,7 @@ function App() {
     <NotificationProvider>
       <ServerConfigProvider>
         <AuthProvider>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
