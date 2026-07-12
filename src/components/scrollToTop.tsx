4e60b543a0c0d4b84e7d96fffc9c74e2c0ac4ea6
Justin Ferrara <justin.ferrara@heig-vd.ch>
2026-07-12 22:14:48 +0200

    refactor: scoll middleware

--- src/components/scrollToTop.tsx
+++ src/components/scrollToTop.tsx
@@ -1,25 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 
 export default function ScrollToTop() {
     const location = useLocation();
-    const prevPath = useRef(location.pathname);
 
     useEffect(() => {
-        // Scroll only if the pathname did NOT change
-        if (prevPath.current === location.pathname) {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-            });
-        } else {
-            // Scroll to top immediately if the pathname changed
-            window.scrollTo(0, 0);
-        }
-        // Update the previous path to the current path
-        prevPath.current = location.pathname;
-    }, [location]);
+        // Scroll to top when the pathname changes
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
 
     return null;
 }
